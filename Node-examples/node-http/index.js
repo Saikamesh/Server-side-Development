db92c21816bbd2ffe981e6959feb7a7c9efe8a5c
Sai1919 <51651413+Saikamesh@users.noreply.github.com>
2020-06-20 22:37:16 +0530

    adding http server support

--- Node-examples/node-http/index.js
+++ Node-examples/node-http/index.js
@@ -1,5 +1,7 @@
 // importing http module
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 //initializing variables
 const hostname = 'localhost';
@@ -8,11 +10,50 @@ const port = 3000;
 //creating server
 
 const server = http.createServer((req, res) => {
-  console.log(req.headers);
+  console.log('Request for ' + req.url + ' by method' + req.method);
+  if (req.method === 'GET') {
+    var fileUrl;
+    if (req.url == '/') fileUrl = '/index.html';
+    else fileUrl = req.url;
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>Hello, World!</h1></body></html>');
+    var filePath = path.resolve('./public' + fileUrl);
+    const fileExt = path.extname(filePath);
+    if (fileExt == '.html') {
+      fs.exists(filePath, (exists) => {
+        if (!exists) {
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/html');
+          res.end(
+            '<html><body><h1>Error 404 ' +
+              fileUrl +
+              'not found</h1></body></html>'
+          );
+          return;
+        }
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        fs.createReadStream(filePath).pipe(res);
+      });
+    } else {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(
+        '<html><body><h1>Error 404 ' +
+          fileUrl +
+          '  not an html file</h1></body></html>'
+      );
+      return;
+    }
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(
+      '<html><body><h1>Error 404 ' +
+        req.method +
+        'not Supported</h1></body></html>'
+    );
+    return;
+  }
 });
 
 server.listen(port, hostname, () => {
