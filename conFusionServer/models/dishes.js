6dc0ca477c2bdd90ac57c340b5e12db8592ec658
Sai1919 <51651413+Saikamesh@users.noreply.github.com>
2020-07-01 17:40:39 +0530

    Express REST API with Mongoose -1

--- conFusionServer/models/dishes.js
+++ conFusionServer/models/dishes.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
 
 const commentSchema = new Schema(
   {
@@ -33,6 +35,27 @@ const dishSchema = new Schema(
     description: {
       type: String,
       require: true,
+    },
+    image: {
+      type: String,
+      require: true,
+    },
+    category: {
+      type: String,
+      require: true,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    price: {
+      type: Currency,
+      require: true,
+      min: 0,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
     },
     comments: [commentSchema],
   },
